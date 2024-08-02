15f6ad1612163806f983722f71be785303c36600
dabryboy <112767864+dabryboy@users.noreply.github.com>
2024-08-02 14:24:39 +0300

    add static, templates

--- restourant/views.py
+++ restourant/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context: dict[str, str] = {
+        'title': 'Home',
+        'content': 'Главная страница - HOME',
+    }
+    return render (request, 'main/index.html', context)
+
+def about(request):
+    return render (request, 'main/about.html')
