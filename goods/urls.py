f0a6a66b3fc2f4b5ed721f42b37f48b908510ac8
dabryboy <112767864+dabryboy@users.noreply.github.com>
2024-09-22 13:48:40 +0300

    did paginator and add search

--- goods/urls.py
+++ goods/urls.py
@@ -4,6 +4,7 @@ from goods import views
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='catalog'),
+    path('<slug:category_slug>/<int:page>/', views.catalog, name='catalog'),
     path('product/<int:product_id>/', views.product, name='product'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
